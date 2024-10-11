9e72b4e6a82b2ad716151a114bee84413e67419d
tounsidu75 <samitns75@gmail.com>
2024-10-11 12:07:33 +0200

    Création d’une première solution

--- fizzbuzz_v1.py
+++ fizzbuzz_v1.py
@@ -2,11 +2,14 @@ def affiche():
     output = ""
     for i in range(1, 101):
         if i % 15 == 0:
-            output += "FrisBee"
+            output += "FrisBee "
         elif i % 3 == 0:
-            output += "Fizz"
+            output += "Fizz "
         elif i % 5 == 0:
-            output += "Buzz"
+            output += "Buzz "
         else:
-            output += str(i)
-    return output
+            output += str(i) + " "
+    return output.strip()  # Pour enlever l'espace final
+
+if __name__ == "__main__":
+    print(affiche())
